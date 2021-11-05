d10781e4f276dfc46d8874d02e15b901c983b5c9
DiscreteL <1752962271@qq.com>
2021-11-05 12:42:18 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 
 Vue.config.productionTip = false
 
@@ -10,3 +15,11 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+      document.title = to.meta.title
+  }
+  next()
+})
